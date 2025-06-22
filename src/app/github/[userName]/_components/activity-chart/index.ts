37b51c3d524562384ddbc68e23e7d9c42ee0f9d5
shibamoto <silverbirder@gmail.com>
2025-06-22 11:10:13 +0900

    refactor: remove unused components and hooks from GitHub user stats

--- src/app/github/[userName]/_components/activity-chart/index.ts
+++
@@ -1,3 +0,0 @@
-export { ActivityChart } from "./activity-chart";
-export { useActivityChart } from "./activity-chart.hook";
-export type { ActivityChartData, UseActivityChartProps } from "./activity-chart.hook";
--- src/app/github/[userName]/_components/language-chart/index.ts
+++
@@ -1,3 +0,0 @@
-export { LanguageChart } from "./language-chart";
-export { useLanguageChart } from "./language-chart.hook";
-export type { LanguageData, LanguageChartData, UseLanguageChartProps } from "./language-chart.hook";
